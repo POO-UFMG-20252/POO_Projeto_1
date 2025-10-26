1f04e20336ce86c623ec0d930aab11c907a56712
lucas-pereira983 <lucasagonpe@gmail.com>
2025-10-26 20:14:40 -0300

    adição de setters e getters para produto

--- backend/src/classes/produto.py
+++ backend/src/classes/produto.py
@@ -5,6 +5,32 @@ class Produto():
 		self.id = id
 		self.nome = nome
 		self.marca = marca
+	#getters e setters nome
+	@property
+	def nome(self):
+		return self.nome
+	@nome.setter
+	def nome(self, nome_novo):
+		self._nome = nome_novo
+
+	#getters e setters marca
+	@property
+	def marca(self):
+		return self.marca
+	@marca.setter
+	def marca(self, marca_nova):
+		self._marca = marca_nova
+	
+	#getter id
+	@property
+	def id(self):
+		return self.id
+	
+	#setter id(opcional)
+	@id.setter
+	def id(self,id_novo):
+		self_id = id_novo
+	
 	
 	@staticmethod
 	def salvar_produto(produto):
